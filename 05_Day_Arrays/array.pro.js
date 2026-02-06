9e7826eca3d3d4b4a2ed71c4914543464f89e41c
filpos <mergetafilpos@gmail.com>
2026-02-06 11:17:54 +0300

    create all 05_Day_Arrays file messages

---
+++ 05_Day_Arrays/array.pro.js
@@ -0,0 +1,55 @@
+/*
+//Example of Arrey
+let name1 = "jhon ";
+let name2 = "berhe";
+let result = name1 + name2;
+console.log(result);
+
+
+//Example 2
+let message = "Helo";
+console.log(message[0]);
+console.log(message[1]);
+console.log(message[2]);
+console.log(message[3]);
+
+//
+let num = prompt('Enter number');
+switch (true) {
+  case num > 0:
+    console.log('Number is positive');
+    break;
+  case num == 0:
+    console.log('Numbers is zero');
+    break;
+  case num < 0:
+    console.log('Number is negative');
+    break;
+  default:
+    console.log('Entered value was not a number');
+}
+    */
+
+// example of if else
+  let x = 8, y= 4;
+if(x < y){
+  console.log("the number is positiv");
+}
+else {
+  console.log("the namber is negative");
+}
+//
+let age = 18;
+
+if(age < 10){
+  console.log("you can not drive!");
+}
+else if(age > 17){
+  console.log("you can drive");
+}
+else{
+  console.log("you are under age");
+}
+//array 
+let name = "hello";
+console.log(name[name.length-1]);
