2cd87e90b798c591850099af6e3604aaa124245a
filpos <mergetafilpos@gmail.com>
2026-02-09 00:46:00 +0300

    solve of maps and set order 10_day messages

---
+++ 10_Day_Sets_and_maps/maps/Adding_to_value_maps.js
@@ -0,0 +1,8 @@
+// Adding values to the Map
+const fullName = new Map();
+console.log(fullName.size);
+fullName.set("Filpos", "Seife");
+fullName.set("Ermias", "master");
+fullName.set("Henok", "Hagose");
+console.log(fullName);
+console.log(fullName.size);
